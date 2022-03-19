8cbd5a03cecdf95a880b142c57306657604015c4
Caio <caiostun@gmail.com>
2022-03-19 18:38:12 -0300

    Envio de Mocaps

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import Navbar from "./components/Navbar/Navbar"
+import Cadastro from "./components/pages/Cadastro/Cadastro"
+import Perfil from "./components/pages/Perfil/Perfil"
+import { Login } from "./components/pages/Login/Login"
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <main>
+        <Navbar />
+        {/*    <Cadastro /> */}
+        <Perfil />
+        {/*      <Login /> */}
+      </main>
+    </>
+  )
+
+}
+
+export default App
